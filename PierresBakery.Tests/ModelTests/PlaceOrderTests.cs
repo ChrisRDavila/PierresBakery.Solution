15f62c7c3922ff585bf993a6446e6b607386a732
ChrisRDavila <crossdavila@gmail.com>
2023-05-05 16:54:13 -0700

    WIP. unsure next move. planning

--- PierresBakery.Tests/ModelTests/PlaceOrderTests.cs
+++ PierresBakery.Tests/ModelTests/PlaceOrderTests.cs
@@ -5,5 +5,19 @@ using System;
 
 namespace PlaceOrder.Tests
 {
-  
+  [TestClass]
+  public class PlaceOrderTests
+  {
+    public void GetLists_GetBothLists_Lists()
+    {
+      //Arrange
+      Bread newBread = new Bread(5, "breadloaf");
+      List<Bread> breadList = new List<Bread> {newBread};
+      Pastry newPastry = new Pastry(2, "pastry");
+      List<Pastry> pastryList = new List<Pastry> {newPastry};
+      //Act
+      
+
+    }
+  }
 }
