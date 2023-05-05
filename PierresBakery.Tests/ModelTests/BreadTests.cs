a2525a3980976afde35fe2cf6a1dfb33f9c599a5
ChrisRDavila <crossdavila@gmail.com>
2023-05-05 10:54:25 -0700

    WIP, trying to plan out testing

--- PierresBakery.Tests/ModelTests/BreadTests.cs
+++ PierresBakery.Tests/ModelTests/BreadTests.cs
@@ -10,19 +10,8 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread testBread = new Bread();
+      Bread testBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), testBread.GetType());
-    }
-
-    public void GetBreadPrice_ReturnPrice_Int()
-    {
-      //Arrange
-      int breadPrice = 5;
-      Bread testBread = new Bread(breadPrice);
-      //Act
-      int result = testBread.BreadPrice;
-      //Assert
-      Assert.AreEqual(breadPrice, result);
     }
   }
 }
--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -4,6 +4,11 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    //
+    public int BreadPrice{ get; set;}
+
+    public Bread(int unit_cost)
+    {
+      BreadPrice = unit_cost;
+    }
   }
 }
