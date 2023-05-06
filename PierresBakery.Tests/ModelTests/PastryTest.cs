f4b7ac8e732c23a5610397510641bc75df3fc129
ChrisRDavila <crossdavila@gmail.com>
2023-05-06 11:35:05 -0700

    test 9 for pastry get value passed

--- PierresBakery.Tests/ModelTests/PastryTest.cs
+++ PierresBakery.Tests/ModelTests/PastryTest.cs
@@ -14,5 +14,15 @@ namespace PierresBakery.Tests
       Pastry testPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), testPastry.GetType());
     }
+    [TestMethod]
+    public void GetPastryAmount_ReturnsPastryAmount_Int()
+    {
+      int testAmount = 5;
+      Pastry testPastry = new Pastry(5);
+      int result = testPastry.Rolls;
+      Assert.AreEqual(testAmount, result);
+
+    }
+
   }
 }    
